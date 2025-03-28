6f786155695f8341437026ab027ad7d6b95d1aef
ThomasWega <tomous.wega@gmail.com>
2025-03-28 20:29:02 +0100

    changed test

--- src/main/java/me/wega/Main.java
+++ src/main/java/me/wega/Main.java
@@ -1,20 +1,26 @@
 package me.wega;
 
+import java.util.HexFormat;
+
 public class Main {
     public static void main(String[] args) {
         chaCha20();
     }
 
     public static void chaCha20() {
-        String key = "ThisIsAKeyForChaCha20CipherUwu11"; // Must be exactly 32 bytes
-        byte[] keyBytes = key.getBytes();
-        // (nonce) - must be 12 bytes
-        String iv = "9B0PMrNzh7k=";
-        byte[] ivBytes = iv.getBytes();
+        byte[] keyBytes = HexFormat.of().parseHex(
+                "00:01:02:03:04:05:06:07:08:09:0a:0b:0c:0d:0e:0f:10:11:12:13:14:15:16:17:18:19:1a:1b:1c:1d:1e:1f"
+                        .replace(":", "")
+        );
+        byte[] ivBytes = HexFormat.of().parseHex(
+                "00:00:00:00:00:00:00:4a:00:00:00:00"
+                        .replace(":", "")
+        );
         int initialCounter = 1;
 
         ChaCha20 chacha = new ChaCha20(keyBytes, ivBytes, initialCounter);
-        byte[] plaintext = "Hello World.".getBytes();
+        byte[] plaintext = "Ladies and Gentlemen of the class of '99: If I could offer you only one tip for the future, sunscreen would be it."
+                .getBytes();
         byte[] ciphertext = chacha.xor(plaintext);
         System.out.println("Encrypted: " + bytesToHex(ciphertext));
 
